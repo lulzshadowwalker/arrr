27ddc212a053039d37a9e8cfaeca0b9819af1fe3
lulzshadowwalker <lulzshadowwalker@gmail.com>
2022-04-27 20:26:27 +0300

    🚀 FEAT : Relative Imports

--- lib/helpers/lulz_imports.dart
+++ lib/helpers/lulz_imports.dart
@@ -1,4 +1,4 @@
-import 'package:arrr/themes/lulz_theme.dart';
+import '../themes/lulz_theme.dart';
 import 'package:flutter/material.dart';
 
 part '../arrr.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:arrr/helpers/lulz_imports.dart';
+import 'helpers/lulz_imports.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const Arrr());
